43827454d944a6eff173f533eb6e808555a82c5e
Nathan <47441022+NathanSnail@users.noreply.github.com>
2025-09-14 19:41:45 +1200

    rifts summon

--- files/scripts/rift_spawn.lua
+++ files/scripts/rift_spawn.lua
@@ -1,9 +1,17 @@
 local me = GetUpdatedEntityID()
 local x, y = EntityGetTransform(me)
-for angle = 0, 360, 20 do
+for angle = 0, 360, 36 do
 	local theta = angle * math.pi / 180
-	local length = 100
+	local length = 160
 	local dx, dy = math.cos(theta) * length, math.sin(theta) * length
 	local nx, ny = x + dx, y + dy
-	EntityLoad("mods/dimensional_rifts/files/entities/rift_portal.xml", nx, ny)
+	-- TODO: this function doesn't work too good, maybe do some raycasts
+	local fx, fy = FindFreePositionForBody(nx, ny, 0, 0, 15)
+	local portal = EntityLoad("mods/dimensional_rifts/files/entities/rift_portal.xml", fx, fy)
+	local tier = math.max(0, math.floor(y / 1024))
+	EntityAddComponent2(
+		portal,
+		"VariableStorageComponent",
+		{ name = "dimensional_rifts.tier", value_int = tier }
+	)
 end
